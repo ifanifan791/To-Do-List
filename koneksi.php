33742c1b825a8e9a0c0cf238831c07a2001d6255
Krnandi <ifanifan791@gmail.com>
2024-10-23 14:46:16 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'kegiatan';
+$databaseUsername = 'root'; // Ensure this is correct
+$databasePassword = ''; // Provide the correct password if necessary
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
